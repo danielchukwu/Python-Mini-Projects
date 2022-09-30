d5dcb1389a13d4161b77917f01b4e0eb930ffc98
dc <00danzy@gmail.com>
2022-09-30 07:50:52 +0100

    add boilerplate

---
+++ 2. Caesar Cipher/CeasarCipher.py
@@ -0,0 +1,27 @@
+# Python - 3.10
+# Topic - caecer cipher
+# Program - write a program that encodes and decodes a message
+
+# print ascii art of caecer cipher
+
+# Type 'encode' to encrypt, type 'decode' to decrypt:
+# encode
+# Type your message:
+# hellocoders
+# Type the shift number:
+# 9
+# Here's the encoded result: qnuuxlxmnbc
+# Type 'yes' if you want to go again. Otherwise type 'no'.
+# yes
+
+# Type 'encode' to encrypt, type 'decode' to decrypt:
+# decode
+# Type your message:
+# qnuuxlxmnbc
+# Type the shift number:
+# 9
+# Here's the decoded result: hellocoders
+# Type 'yes' if you want to go again. Otherwise type 'no'.
+
+
+# solution
