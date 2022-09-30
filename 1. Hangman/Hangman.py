9a0291ef02465cb562aecbaad64ebf02367704ff
dc <00danzy@gmail.com>
2022-09-30 07:49:03 +0100

    add boilerplate

---
+++ 1. Hangman/Hangman.py
@@ -0,0 +1,18 @@
+# Python - 3.10
+# Topic - Print & Input
+# Program - Write a program of hangman
+
+# beekeepe_
+#  +---+
+#  |   |
+#  0   |
+# /|\  |
+# / \  |
+#      |
+# ========
+
+# Guess a letter: 
+
+
+# solution
+
